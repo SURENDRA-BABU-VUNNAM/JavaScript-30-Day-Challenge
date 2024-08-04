deb82b0e884b8771e7bd8456f21b2688131bf52f
SURENDRA-BABU_VUNNAM <vunnam.surendrababu@gmail.com>
2024-08-04 22:34:27 +0530

    leet code medium task 2 and 3

--- 22_Day_22_leet_code_medium/01_tasks_and_activities/02_longest_sub_string/02_task_2.js
+++ 22_Day_22_leet_code_medium/01_tasks_and_activities/02_longest_sub_string/02_task_2.js
@@ -4,3 +4,25 @@
 
 // Log the length for a few test cases, including edge cases.
 
+function longest_substring(str) {
+    let char_set = new Set()
+    let left = 0
+    let max_length = 0
+    let max_substring = ""
+    
+    for (let right = 0; right <= (str.length - 1); right++){
+        while (char_set.has(str[right])) {
+            char_set.delete(str[left])
+            left++
+        }
+        char_set.add(str[right])
+        if (right-left+1 > max_length) {
+            max_length = Math.max(max_length, right - left + 1);
+            max_substring = str.substring(left,right+1)
+        }
+    }
+    console.log(max_substring)
+    return max_length
+}
+
+console.log(longest_substring("surendra"))
--- 22_Day_22_leet_code_medium/01_tasks_and_activities/03_most_water/03_task_3.js
+++ 22_Day_22_leet_code_medium/01_tasks_and_activities/03_most_water/03_task_3.js
@@ -1,4 +1,28 @@
 // Task 3: Solve the "Container With Most Water" problem on LeetCode.
+
 // Write a function that takes an array of non-negative integers where each integer represents the height of a line drawn at each point. Find two lines that together with the x-axis form a container, such that the container holds the most water.
+
 // Log the maximum amount of water for a few test cases.
 
+function max_height(array) {
+    let left = 0
+    let right = array.length - 1
+    let max_area = 0
+    while (left < right) {
+        let width = right - left
+        let min_height = Math.min(array[left], array[right])
+        let area = width * min_height
+        max_area = Math.max(max_area, area)
+        
+        if (array[left] < array[right]) {
+            left++;
+        } else {
+            right--;
+        }
+
+    }
+    return max_area
+}
+
+let arr = [10,20,30,40]
+console.log(max_height(arr));
