dc5ce1f0db5895f310ee83e3ae52a747b08f4782
SURENDRA-BABU_VUNNAM <vunnam.surendrababu@gmail.com>
2024-08-21 19:34:43 +0530

    classes revisit started

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
+class Person {
+    constructor(name,age) {
+        this.name = name;
+        this.age = age
+    }
+    greet() {
+        console.log(`the my name is ${this.name} and age is ${this.age}`);
+    }
+  }
+
+class Students extends Person {
+    constructor(name, age,major) {
+        super(name, age)
+        this.major = major
+    }
+    study() {
+        console.log(`I am studying ${this.major}`);
+    }
+}
 
 
+let person_1 = new Students("raj", 27, "physics")
+
+person_1.study()
+person_1.greet()
 
